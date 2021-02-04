e8d50071f28f196929d55c342f8cd357d2894b75
DanVu <danvu@ya.ru>
2021-02-04 18:04:37 +0300

    added selenide

--- src/test/java/DemoqaTests.java
+++ src/test/java/DemoqaTests.java
@@ -16,53 +16,51 @@ public class DemoqaTests {
     static void setup() {
         Configuration.startMaximized = true;
     }
+
     @Test
     void textFields() {
-        open("https://demoqa.com/automation-practice-form");    // Open URL
+        open("https://demoqa.com/automation-practice-form");
         $(".main-header").shouldHave(text("Practice Form"));
 
-        $("#firstName").setValue("Dan");                            // Type name
-        $("#lastName").setValue("Vu");                              // Type lastname
-        $("#userEmail").setValue("danvu@ya.ru");                    // Type email
+        $("#firstName").setValue("Dan");
+        $("#lastName").setValue("Vu");
+        $("#userEmail").setValue("danvu@ya.ru");
 
-
-        $("label[for='gender-radio-3']").click();                   // Click radio button
-        $("#userNumber").setValue("1234567890");                    // Set mobile
-
+        $("label[for='gender-radio-3']").click();
+        $("#userNumber").setValue("1234567890");
 
         $("#dateOfBirthInput").click();
-
         $(".react-datepicker__month-select").selectOption("April");
         $(".react-datepicker__year-select").selectOption("1975");
         $("[aria-label='Choose Monday, April 14th, 1975']").click();
 
-        $("#subjectsInput").setValue("hi").pressEnter();
+        $("#subjectsInput").setValue("Hindi").pressEnter();
         $("label[for='hobbies-checkbox-1']").click();
         $("label[for='hobbies-checkbox-2']").click();
         $("label[for='hobbies-checkbox-3']").click();
 
-        File file = new File("src/test/java/resources/ejik.jpg");
+        File file = new File("src/test/resources/ejik.jpg");
         $("#uploadPicture").uploadFile(file);
 
-        $("#currentAddress").setValue("test"); // Тут адрес
-
+        $("#currentAddress").setValue("test");
         $("#react-select-3-input").setValue("N").pressEnter();
         $("#react-select-4-input").setValue("N").pressEnter();
 
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $x("//div[@class='modal-content']").shouldHave(text("Dan Vu"));
 
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[1]/td[2]")).shouldHave(text("Dan Vu"));           //Name
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[2]/td[2]")).shouldHave(text("danvu@ya.ru"));     //mail
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[3]/td[2]")).shouldHave(text("Other"));           //Gender
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[4]/td[2]")).shouldHave(text("1234567890"));      //mobile
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[5]/td[2]")).shouldHave(text("14 April,1975"));   // Birth
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[6]/td[2]")).shouldHave(text("Hindi"));   //Subjects
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[7]/td[2]")).shouldHave(text("Sports, Reading, Music"));  //Hobbies
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[8]/td[2]")).shouldHave(text("ejik.jpg")); // Picture
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[9]/td[2]")).shouldHave(text("test")); //address
-        $(By.xpath("/html/body/div[3]/div/div/div[2]/div/table/tbody/tr[10]/td[2]")).shouldHave(text("NCR Gurgaon")); //state and city
 
-                    }
+        $x("//div[@class='table-responsive']").shouldHave(text("danvu@ya.ru"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Other"));
+        $x("//div[@class='table-responsive']").shouldHave(text("1234567890"));
+        $x("//div[@class='table-responsive']").shouldHave(text("14 April,1975"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Hindi"));
+        $x("//div[@class='table-responsive']").shouldHave(text("Sports, Reading, Music"));
+        $x("//div[@class='table-responsive']").shouldHave(text("ejik.jpg"));
+        $x("//div[@class='table-responsive']").shouldHave(text("test"));
+        $x("//div[@class='table-responsive']").shouldHave(text("NCR Gurgaon"));
+
     }
+}
